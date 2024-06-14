14e22f5de2b80ba75d59dfb3cdf24dfe3b824588
Randy Heiland <randy.heiland@gmail.com>
2024-06-13 20:42:37 -0400

    more sim schemas

--- scripts/generate_html_table.py
+++ scripts/generate_html_table.py
@@ -4,8 +4,13 @@ import pandas as pd
 
 def load_instances(directory):
     instances = []
-    base_url = "https://raw.githubusercontent.com/OpenVT/OpenVTschema/main/"
-    for filename in os.listdir(directory):
+    # base_url = "https://raw.githubusercontent.com/OpenVT/OpenVTschema/main/"
+    base_url = "https://raw.githubusercontent.com/rheiland/OpenVTschema/main/"
+    # for filename in os.listdir(directory):
+    # artistoo.json		compucell3d.json	netlogo.json		simucell3d.json
+    # biocellion.json		hal.json		physicell.json		tissue_forge.json
+    # chaste.json		morpheus.json		polyhoop.json
+    for filename in ["compucell3d.json", "physicell.json", "chaste.json", "morpheus.json", "tissue_forge.json", "biocellion.json", "artistoo.json", "simucell3d.json", "hal.json", "netlogo.json", "polyhoop.json"]:
         if filename.endswith(".json"):
             filepath = os.path.join(directory, filename)
             with open(filepath, 'r') as file:
@@ -21,9 +26,11 @@ def generate_table(instances):
         name = instance.get("name", "N/A")
         description = instance.get("description", "N/A")
         website = instance.get("website", "N/A")
-        schema_link = instance.get("schema_link", "N/A")
-        data.append([name, description, website, schema_link])
-    return pd.DataFrame(data, columns=["Name", "Description", "Website", "Schema"])
+        # schema_link = instance.get("schema_link", "N/A")
+        # data.append([name, description, website, schema_link])
+        data.append([name, description, website])
+    # return pd.DataFrame(data, columns=["Name", "Description", "Website", "Schema"])
+    return pd.DataFrame(data, columns=["Name", "Description", "Website"])
 
 def save_table_as_html(df, file_path):
     if df.empty:
@@ -31,8 +38,9 @@ def save_table_as_html(df, file_path):
         return
 
     # Make website and schema links clickable
-    df['Website'] = df['Website'].apply(lambda x: f'<a href="{x}">{x}</a>')
-    df['Schema'] = df['Schema'].apply(lambda x: f'<a href="{x}">Link</a>')
+    # df['Website'] = df['Website'].apply(lambda x: f'<a href="{x}">{x}</a>')
+    df['Website'] = df['Website'].apply(lambda x: f'<a href="{x}">link</a>')
+    # df['Schema'] = df['Schema'].apply(lambda x: f'<a href="{x}">Link</a>')
 
     # Create HTML table
     html_table = df.to_html(escape=False, index=False)
@@ -47,6 +55,9 @@ def save_table_as_html(df, file_path):
             border-collapse: collapse;
             padding: 10px;
             text-align: left;
+            max-width: 800px;
+            margin-left: auto;
+            margin-right: auto;
         }}
         th {{
             background-color: black;
@@ -55,7 +66,7 @@ def save_table_as_html(df, file_path):
     </style>
     </head>
     <body>
-    <h1>Simulators</h1>
+    <h1>Multicellular Frameworks</h1>
     {html_table}
     </body>
     </html>
@@ -73,6 +84,7 @@ def main():
     instances = load_instances(instances_directory)
     df = generate_table(instances)
     save_table_as_html(df, output_path)
+    print("--> ",output_path)
 
 if __name__ == "__main__":
     main()
