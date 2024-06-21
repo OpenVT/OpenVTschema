508327572ccb3225c5d05783a3992cf74e85a14a
Eran <agmon.eran@gmail.com>
2024-06-21 16:00:06 -0400

    return compucell schema to new style, add workflow to create graphs

--- scripts/generate_graph.py
+++ scripts/generate_graph.py
@@ -7,6 +7,7 @@ def load_instance(file_path):
         return json.load(file)
 
 def create_graph(instance):
+    print(f'Creating graph for {instance["name"]}')
     dot = Digraph(comment=instance['name'])
 
     # Add objects
@@ -15,7 +16,7 @@ def create_graph(instance):
         dot.node(obj['object_id'], obj_label, shape='ellipse')
 
         # Add processes
-        for proc_id in obj['processes']:
+        for proc_id in obj.get('processes', []):
             proc = next((p for p in instance['processes'] if p['process_id'] == proc_id), None)
             if proc:
                 proc_label = f"{proc['name']}\n{proc['description']}"
@@ -23,7 +24,7 @@ def create_graph(instance):
                 dot.edge(obj['object_id'], proc['process_id'], label="uses")
 
         # Add boundary conditions
-        for bc_id in obj['boundary_conditions']:
+        for bc_id in obj.get('boundary_conditions', []):
             bc = next((bc for bc in instance['boundaries']['pde_boundary_conditions'] if bc['bc_id'] == bc_id), None)
             if bc:
                 bc_label = f"{bc['type']} BC\nValue: {bc['value']}"
@@ -37,7 +38,7 @@ def create_graph(instance):
                     dot.edge(obj['object_id'], bc['gc_id'], label="adheres to")
 
     # Add containment relationships
-    for containment in instance['containment']:
+    for containment in instance.get('containment', []):
         dot.edge(containment['outer_object_id'], containment['inner_object_id'], label="contains")
 
     return dot
@@ -46,12 +47,19 @@ def save_graph(dot, file_path):
     dot.render(file_path, format='png')
 
 def main():
-    instance_file = '../simulator_schemas/compucell3d.json'  # Path to your CompuCell3D instance JSON file
-    output_file = '../docs/compucell3d_graph'
+    instances_directory = '../simulator_schemas'
+    output_directory = '../docs'
 
-    instance = load_instance(instance_file)
-    dot = create_graph(instance)
-    save_graph(dot, output_file)
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    for filename in os.listdir(instances_directory):
+        if filename.endswith(".json"):
+            instance_file = os.path.join(instances_directory, filename)
+            instance = load_instance(instance_file)
+            dot = create_graph(instance)
+            output_file = os.path.join(output_directory, f"{os.path.splitext(filename)[0]}_graph")
+            save_graph(dot, output_file)
 
 if __name__ == "__main__":
     main()
